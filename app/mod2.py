33ef3a5b5e191edb1b898c344b9929ded02fcb21
Ewuruakua419 <ewurakua.amoah@ashesi.edu.gh>
2026-07-08 21:32:10 +0000

    add mod3

--- app/mod2.py
+++ app/mod2.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import uuid
+import json
 class Wallet:
     def __init__(self,balance,user:'User'):
         self.balance=balance
@@ -12,6 +13,7 @@ class Wallet:
     
     def history(self, statement:str):
         self.hist.extend(statement)
+        
 
     def gethist(self):
         return self.hist
@@ -39,12 +41,12 @@ class Wallet:
             self.history(histlist)
             return self.balance
         
-    def transfer(self, amt:int, wll:'User'):
-        wll.wallet.deposit(amt)
+    def transferin(self, amt:int, reciever:'User'):
+        reciever.wallet.deposit(amt)
         self.withdraw(amt)
 
-        self.transactions.append(Transaction(reciever=wll.name,sender=self.user.name, amount=amt, types='Withdraw'))
-        wll.wallet.transactions.append(Transaction(reciever=wll.name,sender=self.user.name, amount=amt, types='Deposit'))
+        self.transactions.append(Transaction(reciever=reciever.name,sender=self.user.name, amount=amt, types='Withdraw'))
+        reciever.wallet.transactions.append(Transaction(reciever=reciever.name,sender=self.user.name, amount=amt, types='Deposit'))
         
 
 class User:
@@ -56,6 +58,19 @@ class User:
     
     def getId(self):
         return self.unique_id
+    
+    def upload(self):
+        with open("users.json", "r") as file:
+            data = json.load(file)
+        if self.user.unique_id not in data:
+                data[self.user.unique_id ] = {
+                    "balance": self.balance,
+                    "history": []
+                }
+        data[self.user.unique_id ]['balance']= self.wallet.balance,
+        data[self.user.unique_id ].setdefault('history',[]).extend(self.wallet.transactions)
+        with open("users.json", "w") as file:
+                json.dump(data, file, indent=4)
 
 class Transaction:
     def __init__(self, sender, reciever,amount, types):
@@ -69,26 +84,59 @@ class Transaction:
 class Bank:
     def __init__(self):
         self.users={}
+        self.curr=None
 
     def adduser(self, user:'User'):
         self.users[user.unique_id]=user
 
-    def finduser(self, id):
-        if id in self.users:
-            print('User found')
-            return True
+    def finduser(self, name):
+        for item in self.users:
+            if name==item.name:
+                print('User found')
+                return item.unique_id
         else:
             print('User not found')
             return False
         
     def register(self, name, password):
+        for item in self.users:
+            if name==item.name:
+                print('User already exists')
+                return False
         user=User(name=name,password=password)
         self.adduser(user=user)
 
-    def login(self, unique_id):
-        if self.finduser(unique_id):
-            print('logged in')
-        else:
-            print('login unsuccessful')
-            return False
+
+    def login(self, name, password):
+        for item in self.users:
+            if name==item.name:
+        
+                if password==item.password:
+                    self.curr=item
+                    print('logged in')
+                
+                print('login unsuccessful:Wrong password')
+                return False
+        print('login unsuccessful:Username not found')
+        return False
+    
+    def transfer(self,name, amt:int):
+        if self.finduser(name)!=False:
+            reciever=self.finduser(name)
+            if self.curr==None:
+                print("login first")
+                return False
+            self.curr.transferin(reciever=reciever,amt=amt)
+
+    def logout(self):
+        if self.curr==None:
+                print("login first")
+                return False
+        self.curr.upload()
+        self.curr=None
+
+
+
+
+        
 
